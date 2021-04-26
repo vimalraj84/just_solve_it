03b62cc0097dac87da4eeb5463e962781775757c
vimalraj <vimalraj84@gmail.com>
2021-04-26 18:55:20 -0400

    new programs

---
+++ solve-it/src/main/java/com/bilqu/solve/prob/SockPairCounter.java
@@ -0,0 +1,29 @@
+package com.bilqu.solve.prob;
+
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class SockPairCounter {
+
+    public static void main(String[] args) {
+        final List<Integer> socks = new ArrayList<>();
+        try (Scanner scanner = new Scanner(System.in)){
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+            String[] ip = scanner.nextLine().split(" ");
+
+            Arrays.stream(ip).forEach(i -> socks.add(Integer.valueOf(i)));
+            int out = countPairs(socks);
+            System.out.println(out);
+        }
+    }
+
+    private static int countPairs(List<Integer> input){
+      Map<Integer,Long> map = input.stream().collect(Collectors.groupingBy(Function.identity(),Collectors.counting()));
+
+          Long out = map.entrySet().stream().filter(e -> e.getValue() > 1).collect(Collectors.counting());
+
+      return out.intValue();
+
+    }
+}
